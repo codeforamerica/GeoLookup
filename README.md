30561933840447371736a04c5116d2b0855eaa5c
Samuel Painter <sam.foy.painter@gmail.com>
2012-06-16 22:19:57 -0400

    now finds coordinates and has README

---
+++ README.md
@@ -0,0 +1,7 @@
+#GeoLookup
+
+GeoLookup is a python script that takes addresses and returns the coordinates
+associated with the address.
+
+GeoLookup uses the Google Maps Geocoding API and your use of this script is
+subject to the terms found at at [](http://developers.google.com/maps/terms)
---
+++ geolookup.py
@@ -0,0 +1,46 @@
+import simplejson, urllib, sys, getopt
+
+GEOCODE_BASE_URL = 'http://maps.googleapis.com/maps/api/geocode/json'
+
+def geocode(address,sensor, **geo_args):
+  geo_args.update({
+    'address': address,
+    'sensor': sensor
+    })
+
+  url = GEOCODE_BASE_URL + '?' + urllib.urlencode(geo_args)
+  result = simplejson.load(urllib.urlopen(url))
+
+  result = simplejson.dumps([s['geometry'] for s in result['results']])
+
+  result = simplejson.loads(result)
+
+  for item in result:
+    print item['location']['lat']
+    print item['location']['lng']
+
+def make_addr(input):
+  return input.replace(' ', '+')
+
+def main(argv):
+  addr = ''
+  sens = 'false'
+  try:
+    opts, args = getopt.getopt(argv,"ha:s:",["address=","sensor="])
+  except getopt.GetoptError:
+    print 'geolookup.py -a <address> -s <sensor>'
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      print 'geolookup.py -a <address> -s <sensor>'
+      sys.exit()
+    elif opt in ("-a", "--address"):
+      addr = make_addr(arg)
+    elif opt in ("-s", "--sensor"):
+      sens = arg
+
+    
+  geocode(address=addr,sensor=sens)
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
